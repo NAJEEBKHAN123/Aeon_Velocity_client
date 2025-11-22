400e3d5d09284398c666be1726caae2710f9763f
NAJEEBKHAN123 <najeebkhanlaku@gmail.com>
2025-11-22 23:04:09 +0500

    remove the bg imag from the sposor

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -325,10 +325,7 @@ const SponsorsPage = () => {
           className={`absolute inset-0 bg-cover bg-center bg-no-repeat transition-opacity duration-1000 ${
             imageLoaded ? "opacity-100" : "opacity-0"
           }`}
-          style={{
-            backgroundImage:
-              'url("https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2825&q=80")',
-          }}
+          
         >
           <img
             src="https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2825&q=80"
